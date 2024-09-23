f505714cb0860ddb499060579be78c7b6dd1172f
34x340 <ascensionbat@gmail.com>
2024-09-23 19:22:12 +0300

    okkk
    а

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,10 +1,16 @@
-function fetchPokemonById(pokemonId) {
-  return (fetch(`https://pokeapi.co/api/v2/pokemon/5`))
-  .then(pokemon=>console.log(pokemon))
-}
+// function fetchPokemonById(pokemonId) {
+//   return (fetch(`https://pokeapi.co/api/v2/pokemon/5`))
+//   .then(pokemon=>console.log(pokemon))
+// }
 
-fetchPokemonById(10)
-  .then(pokemon => {
-      document.body.textContent = pokemon.name
-  })
+// fetchPokemonById(10)
+//   .then(pokemon => {
+//       document.body.textContent = pokemon.name
+//   })
 
+searchArticles('flower')
+    .then(res => res.articles)
+    .then(articles => {
+        const markup = createArticleCards(articles)
+        articlesContainerRef.innerHTML = markup
+    })
