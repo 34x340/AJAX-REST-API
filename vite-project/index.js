b1fbad12c47db8b20a6e47c1415b1a6b91a3aecc
34x340 <ascensionbat@gmail.com>
2024-09-23 20:35:05 +0300

    parc

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,16 +1,66 @@
-// function fetchPokemonById(pokemonId) {
-//   return (fetch(`https://pokeapi.co/api/v2/pokemon/5`))
-//   .then(pokemon=>console.log(pokemon))
-// }
+import { NewsApiService } from "./api-service";
 
-// fetchPokemonById(10)
-//   .then(pokemon => {
-//       document.body.textContent = pokemon.name
-//   })
+const formRef = document.querySelector('.js-search-form');
+const articlesContainerRef = document.querySelector('.js-articles-container');
+const loagMoreBtnRef = document.querySelector('[data-action="load-more"]')
 
-searchArticles('flower')
+
+const newsApiService = new NewsApiService()
+console.log(newsApiService);
+
+formRef.addEventListener('submit', onSearch)
+loagMoreBtnRef.addEventListener('click', onLoadMore)
+
+
+
+let page = 1;
+let searchQuery;
+
+
+console.log('page до сабміту', page);
+
+
+function createArticleCards(articles) {
+    return articles.map(article => {
+        return `
+        <li>
+            <a href="${article.url}" target="_blank" rel="noopener noreferrer">
+            <article>
+                <img src="${article.urlToImage}" alt="" width="480">
+                <h2>${article.title}</h2>
+                <p>Posted by: ${article.author}</p>
+                <p>${article.description}</p>
+            </article>
+            </a>
+        </li>
+        `
+    })
+}
+
+
+formRef.addEventListener('submit', onSearch)
+function onSearch(event){
+    event.preventDefault() 
+   newsApiService.searchQuery = event.currentTarget.elements.query.value
+       newsApiService.searchArticles()
     .then(res => res.articles)
     .then(articles => {
         const markup = createArticleCards(articles)
-        articlesContainerRef.innerHTML = markup
+        articlesContainerRef.insertAdjacentHTML('beforeend', markup)
+        newsApiService.page += 1;
+        console.log('page до сабміту', page);
     })
+    form.reset()
+}
+
+
+function onLoadMore() {
+    newsApiService.searchArticles()
+        .then(res => res.articles)
+        .then(articles => {
+            const markup = createArticleCards(articles)
+            articlesContainerRef.insertAdjacentHTML('beforeend', markup)
+            newsApiService.page += 1;
+            console.log('page до сабміту', page);
+        })
+}
